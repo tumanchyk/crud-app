2cee54060d2552a135de7246e37453a6c8fbc946
Tetiana Tsar <protetsar@gmail.com>
2023-12-03 12:36:06 +0200

    add hook useCurrentList

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,32 +1,12 @@
-import {useEffect, useState} from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
+import useCurrentList from '../../hooks/useCurrentList';
 import ListItem from "./ListItem/ListItem";
 import { ListEl, HeadList, HeadItem, NoItems } from "./List.styled";
-import { getAllPlaces } from '../../store/places/placesOperations';
 
 const List = () => {
-    const [currentList, setCurrentList] = useState([]);
-    const dispatch = useDispatch();
-
-    const list = useSelector(state => state.places.list);
     const typeList = useSelector(state => state.places.type);
-    
-    useEffect(() => {
-        dispatch(getAllPlaces())  
-    }, [dispatch])
-
-    useEffect(() => {
-        if (typeList === "visited") {
-            const newList = list.filter(item => item.isVisited)
-            setCurrentList(newList)
-        } else if (typeList === "not visited") {
-            const newList = list.filter(item => !item.isVisited)
-            setCurrentList(newList)
-        } else {
-            setCurrentList(list)
-        }
-    }, [typeList, list])
-
+    const currentList = useCurrentList();
+   
     return <>
         { currentList.length <= 0 ? <NoItems>Any {typeList === "all" ? "" : typeList} locations in your list</NoItems>
         : <>
